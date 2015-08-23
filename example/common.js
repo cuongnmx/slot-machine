24473be5dc542d4c30dba6babb91bafffb590966
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2015-08-22 20:18:57 -0700

    :tangerine: added fruits example :watermelon:

--- example/common.js
+++ example/common.js
@@ -1,24 +1,24 @@
 var SlotMachine = require('../src/slot-machine');
 
-var reelContainer = document.getElementById('container-reel');
-var startButton = document.getElementById('button-start');
-var resultContainer = document.getElementById('result');
+var reelContainerCoffee = document.getElementById('container-reel-coffee');
+var startButtonCoffee = document.getElementById('button-start-coffee');
+var resultContainerCoffee = document.getElementById('result-coffee');
 
-var reel1 = document.getElementById('reel-1');
-var reel2 = document.getElementById('reel-2');
-var reel3 = document.getElementById('reel-3');
+var reelCoffee1 = document.getElementById('reel-coffee-1');
+var reelCoffee2 = document.getElementById('reel-coffee-2');
+var reelCoffee3 = document.getElementById('reel-coffee-3');
 
-var slotMachine = new SlotMachine({
+var slotMachineCoffee = new SlotMachine({
   reels: [
       ['coffee maker', 'teapot', 'espresso machine'],
       ['coffee filter', 'tea strainer', 'espresso tamper'],
       ['coffee grounds', 'loose tea', 'ground espresso beans']
     ],
   sequence: ['coffee', 'tea', 'expresso'],
-  reelNodes: [ reel1, reel2, reel3],
-  reelContainer: reelContainer,
-  startButton: startButton,
-  resultContainer: resultContainer,
+  reelNodes: [reelCoffee1, reelCoffee2, reelCoffee3],
+  reelContainer: reelContainerCoffee,
+  startButton: startButtonCoffee,
+  resultContainer: resultContainerCoffee,
   slotHeight: 100,
   text: {
     jackpot: 'Congratulations, you won a free {drink}.',
@@ -30,4 +30,36 @@ var slotMachine = new SlotMachine({
   }
 });
 
-slotMachine.init();
+slotMachineCoffee.init();
+
+var reelContainerFruit = document.getElementById('container-reel-fruit');
+var startButtonFruit = document.getElementById('button-start-fruit');
+var resultContainerFruit = document.getElementById('result-fruit');
+
+var reelFruit1 = document.getElementById('reel-fruit-1');
+var reelFruit2 = document.getElementById('reel-fruit-2');
+var reelFruit3 = document.getElementById('reel-fruit-3');
+
+var slotMachineFruit = new SlotMachine({
+  reels: [
+      ['apple', 'orange', 'papaya'],
+      ['apple', 'orange', 'papaya'],
+      ['apple', 'orange', 'papaya']
+    ],
+  sequence: ['apple', 'orange', 'papaya'],
+  reelNodes: [reelFruit1, reelFruit2, reelFruit3],
+  reelContainer: reelContainerFruit,
+  startButton: startButtonFruit,
+  resultContainer: resultContainerFruit,
+  slotHeight: 100,
+  text: {
+    jackpot: 'Congratulations, you won a free {drink}.',
+    tryAgain: 'Better luck next time.'
+  },
+  css: {
+    jackpot: 'jackpot',
+    result: 'result'
+  }
+});
+
+slotMachineFruit.init();
